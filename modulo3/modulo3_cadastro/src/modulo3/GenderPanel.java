300e51985eb611097fc315c2c40002ac829ca4cf
pemcauwilla <pamicw07@gmail.com>
2024-11-10 12:39:34 -0300

    Changes in modulo3_cadastro

--- modulo3/modulo3_cadastro/src/modulo3/GenderPanel.java
+++ modulo3/modulo3_cadastro/src/modulo3/GenderPanel.java
@@ -2,6 +2,7 @@ package modulo3;
 
 import java.awt.FlowLayout;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
@@ -10,8 +11,15 @@ public class GenderPanel extends JPanel{
 	public GenderPanel() {
 		setLayout(new FlowLayout(FlowLayout.LEFT));
 		
+		JRadioButton masc = new JRadioButton("masculino");
+		JRadioButton fem = new JRadioButton("feminino");
+		
+		ButtonGroup group = new ButtonGroup();
+		group.add(masc);
+		group.add(fem);
+		
 		add(new JLabel("Sexo: "));
-		add(new JRadioButton("masculino"));
-		add(new JRadioButton("feminino"));
+		add(masc);
+		add(fem);
 	}
 }
